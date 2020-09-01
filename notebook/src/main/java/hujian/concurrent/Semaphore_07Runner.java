22a357c5f97654c97a9f845beab4f26e6519386a
hujian <359187466@qq.com>
2020-09-01 23:32:33 +0800

    "增加定时任务"

--- notebook/src/main/java/hujian/concurrent/Semaphore_07Runner.java
+++ notebook/src/main/java/hujian/concurrent/Semaphore_07Runner.java
@@ -1,12 +1,19 @@
 package hujian.concurrent;
 
 import java.util.concurrent.Semaphore;
+import java.util.concurrent.locks.ReentrantLock;
 
+/**
+ * 拿多少锁资源 释放多少资源
+ * @author 35918
+ *
+ */
 public class Semaphore_07Runner {
 	public static void main(String[] args) throws InterruptedException {
 		Semaphore s = new Semaphore(10);
-		s.acquire();
-		s.release();
+		ReentrantLock lock =new ReentrantLock();
+		s.acquireUninterruptibly(10);
+		s.acquire(10);
 	}
 
 }
