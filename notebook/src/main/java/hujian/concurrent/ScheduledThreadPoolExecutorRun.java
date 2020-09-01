5bf36d3313962936af3fc5672529729340da7081
hujian <359187466@qq.com>
2020-09-01 23:33:26 +0800

    1

---
+++ notebook/src/main/java/hujian/concurrent/ScheduledThreadPoolExecutorRun.java
@@ -0,0 +1,12 @@
+package hujian.concurrent;
+
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+
+public class ScheduledThreadPoolExecutorRun {
+public static void main(String[] args) {
+	ScheduledThreadPoolExecutor p = new ScheduledThreadPoolExecutor(2);
+	//p.schedule(command, delay, unit);
+	//p.scheduleAtFixedRate(command, initialDelay, period, unit);
+	//p.scheduleWithFixedDelay(command, initialDelay, delay, unit)
+}
+}
